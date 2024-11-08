2d480d28759b1721a227e698534b5d709f8ef9e8
Paulo Pinto <paulo.pinto@automattic.com>
2024-11-08 15:09:03 +0000

    Add navigation message and toolbar

--- src/ui/import/ImportPages.tsx
+++ src/ui/import/ImportPages.tsx
@@ -1,3 +1,18 @@
+import { Toolbar } from '@/ui/components/Toolbar';
+
 export function ImportPages() {
-	return <>import pages</>;
+	return (
+		<>
+			<Toolbar>
+				<button
+					onClick={ async () => {
+						// TODO
+					} }
+				>
+					Continue
+				</button>
+			</Toolbar>
+			<>Please navigate to a page that shows the navigation menu</>
+		</>
+	);
 }
