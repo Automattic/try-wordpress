89cc2145b672e8b649bd6e73fdf33372e674a4f1
Paulo Pinto <paulo.pinto@automattic.com>
2024-11-12 15:09:05 +0000

    Add toolbar component for page import flow

---
+++ src/ui/import/pages/Toolbar.tsx
@@ -0,0 +1,25 @@
+import { Toolbar as BaseToolbar } from '@/ui/components/Toolbar';
+import { useNavigate } from 'react-router-dom';
+
+export function Toolbar( props: {
+	canContinue: boolean;
+	continueUrl: string;
+	backUrl?: string;
+} ) {
+	const { canContinue, continueUrl, backUrl } = props;
+	const navigate = useNavigate();
+
+	return (
+		<BaseToolbar>
+			{ ! backUrl ? undefined : (
+				<button onClick={ () => navigate( backUrl ) }>Back</button>
+			) }
+			<button
+				disabled={ ! canContinue }
+				onClick={ () => navigate( continueUrl ) }
+			>
+				Continue
+			</button>
+		</BaseToolbar>
+	);
+}
