99be84df1faebd48cfe6157c4b9844650645ec5b
Paulo Pinto <paulo.pinto@automattic.com>
2024-11-12 14:52:23 +0000

    Move interaction with content script to specific component

--- src/ui/import/pages/ImportPagesFlow.tsx
+++ src/ui/import/pages/ImportPagesFlow.tsx
@@ -1,15 +1,9 @@
 import { Toolbar } from '@/ui/components/Toolbar';
 import { Screens } from '@/ui/App';
 import { useNavigate, useParams } from 'react-router-dom';
-import { ContentEventHandler } from '@/ui/blueprints/ContentEventHandler';
-import { EventTypes } from '@/bus/Event';
-import { useEffect } from 'react';
-import { CommandTypes, sendCommandToContent } from '@/bus/Command';
 import { SelectPages } from '@/ui/import/pages/SelectPages';
 import { useSessionContext } from '@/ui/session/SessionProvider';
 import { ImportPage } from '@/ui/import/pages/ImportPage';
-import { useNavigationHtml } from '@/ui/import/pages/useNavigationHtml';
-import { useSelectedPages } from '@/ui/import/pages/useSelectedPages';
 import { SelectNavigation } from '@/ui/import/pages/SelectNavigation';
 
 /* eslint-disable @typescript-eslint/no-shadow */
@@ -27,27 +21,6 @@ export function ImportPagesFlow() {
 	const pageIndex = parseInt( params.page! ?? 0, 10 );
 	const { session } = useSessionContext();
 	const navigate = useNavigate();
-	const [ , setNavigationHtml ] = useNavigationHtml();
-	const [ , setSelectedPages ] = useSelectedPages();
-
-	// Enable or disable highlighting in source site, if step requires it.
-	useEffect( () => {
-		const type =
-			step === Steps.SelectNavigation
-				? CommandTypes.SwitchToNavigationSelectionMode
-				: CommandTypes.SwitchToDefaultMode;
-		void sendCommandToContent( { type, payload: {} } );
-	}, [ step ] );
-
-	// Disable highlighting on unmount.
-	useEffect( () => {
-		return () => {
-			void sendCommandToContent( {
-				type: CommandTypes.SwitchToDefaultMode,
-				payload: {},
-			} );
-		};
-	}, [] );
 
 	let element = <></>;
 	switch ( step ) {
@@ -108,18 +81,6 @@ export function ImportPagesFlow() {
 					Continue
 				</button>
 			</Toolbar>
-			<ContentEventHandler
-				eventType={ EventTypes.OnElementClick }
-				onEvent={ async ( event ) => {
-					void sendCommandToContent( {
-						type: CommandTypes.SwitchToDefaultMode,
-						payload: {},
-					} );
-					setNavigationHtml( ( event.event.payload as any ).content );
-					setSelectedPages( undefined );
-					navigate( Screens.importPages( session.id, step + 1 ) );
-				} }
-			/>
 			{ element }
 		</>
 	);
--- src/ui/import/pages/SelectNavigation.tsx
+++ src/ui/import/pages/SelectNavigation.tsx
@@ -1,4 +1,38 @@
+import { useNavigationHtml } from '@/ui/import/pages/useNavigationHtml';
+import { useSelectedPages } from '@/ui/import/pages/useSelectedPages';
+import { EventTypes } from '@/bus/Event';
+import { CommandTypes, sendCommandToContent } from '@/bus/Command';
+import { Screens } from '@/ui/App';
+import { ContentEventHandler } from '@/ui/blueprints/ContentEventHandler';
+import { useSessionContext } from '@/ui/session/SessionProvider';
+import { useNavigate } from 'react-router-dom';
+import { Steps } from '@/ui/import/pages/ImportPagesFlow';
+import { useEffect } from 'react';
+
 export function SelectNavigation() {
+	const { session } = useSessionContext();
+	const navigate = useNavigate();
+	const [ , setNavigationHtml ] = useNavigationHtml();
+	const [ , setSelectedPages ] = useSelectedPages();
+
+	// Enable highlighting in source site.
+	useEffect( () => {
+		void sendCommandToContent( {
+			type: CommandTypes.SwitchToNavigationSelectionMode,
+			payload: {},
+		} );
+	}, [] );
+
+	// Disable highlighting on unmount.
+	useEffect( () => {
+		return () => {
+			void sendCommandToContent( {
+				type: CommandTypes.SwitchToDefaultMode,
+				payload: {},
+			} );
+		};
+	}, [] );
+
 	return (
 		<>
 			<p>Click on one of the entries of the navigation menu.</p>
@@ -6,6 +40,23 @@ export function SelectNavigation() {
 				If the menu is not shown on screen, click the Back button and
 				then open the menu.
 			</p>
+			<ContentEventHandler
+				eventType={ EventTypes.OnElementClick }
+				onEvent={ async ( event ) => {
+					void sendCommandToContent( {
+						type: CommandTypes.SwitchToDefaultMode,
+						payload: {},
+					} );
+					setNavigationHtml( ( event.event.payload as any ).content );
+					setSelectedPages( undefined );
+					navigate(
+						Screens.importPages(
+							session.id,
+							Steps.SelectPagesFromNavigation
+						)
+					);
+				} }
+			/>
 		</>
 	);
 }
