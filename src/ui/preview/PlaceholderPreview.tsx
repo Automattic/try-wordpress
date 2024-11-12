09f32db3ddbd86ee900eb4645594d56efc7ee58e
ashfame <mail@ashfame.com>
2024-11-12 14:04:52 +0400

    make linter happy

--- src/ui/preview/PlaceholderPreview.tsx
+++ src/ui/preview/PlaceholderPreview.tsx
@@ -1,20 +1,22 @@
 export function PlaceholderPreview() {
 	return (
-		<div style={{ 
-			width: '100%', 
-			height: '100%', 
-			display: 'flex', 
-			justifyContent: 'center', 
-			alignItems: 'center' 
-		}}>
-			<img 
+		<div
+			style={ {
+				width: '100%',
+				height: '100%',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			} }
+		>
+			<img
 				src="icons/icon-128.png"
 				alt="Icon"
-				style={{ 
+				style={ {
 					display: 'block',
 					width: '100px',
-					height: '100px'
-				}}
+					height: '100px',
+				} }
 			/>
 		</div>
 	);
