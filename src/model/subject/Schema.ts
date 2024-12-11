d0d2b7b24b89c6dcbd0e1bb5dfe8b7811b9b8aaa
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-11 16:16:02 +0000

    Add ts function to retrieve the schema of a subject type

---
+++ src/model/subject/Schema.ts
@@ -0,0 +1,9 @@
+import Schema from '@schema/schema.json';
+import { SubjectType } from '@/model/subject/Subject';
+
+export function getSchema( subjectType: SubjectType ) {
+	if ( ! Schema.hasOwnProperty( subjectType ) ) {
+		throw new Error( `Unknown subjectType: ${ subjectType }` );
+	}
+	return Schema[ subjectType ];
+}
