ec9400eb8149578909c98b0a1122b22e7fcddd2a
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-16 15:59:21 +0000

    Add ManualSubjectTypes

--- src/model/Subject.ts
+++ src/model/Subject.ts
@@ -1,5 +1,11 @@
 import { Field } from '@/model/field/Field';
 
+// Some subject types have a custom import UI, where the user manually imports each subject of that type.
+// These types are listed here so that we can centralize their slugs, and navigate to places in the code where they are referenced.
+export enum ManualSubjectTypes {
+	Page = 'page',
+}
+
 export enum SubjectType {
 	BlogPost = 'blog-post',
 	Page = 'page',
