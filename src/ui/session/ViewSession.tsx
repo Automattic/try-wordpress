8a7a7f9fdfaa3ced784ae3a8ba3eaf53b6701596
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-16 14:46:19 +0000

    Show import buttons for all registered schemas

--- src/ui/session/ViewSession.tsx
+++ src/ui/session/ViewSession.tsx
@@ -3,21 +3,31 @@ import { useNavigate } from 'react-router-dom';
 import { Screens } from '@/ui/App';
 import { SubjectType } from '@/model/Subject';
 import { Button } from '@wordpress/components';
+import { getSchemas } from '@/model/Schema';
+
+const schemas = getSchemas();
 
 export function ViewSession() {
 	const { session } = useSessionContext();
 	const navigate = useNavigate();
 
-	const importButtons = [
-		{
-			text: 'Import Posts',
-			url: Screens.blueprints.new( session.id, SubjectType.BlogPost ),
-		},
-		{
-			text: 'Import Pages',
-			url: Screens.importPagesStart( session.id ),
-		},
-	];
+	const importButtons: { text: string; url: string }[] = [];
+	Object.keys( schemas ).forEach( ( subjectType ) => {
+		// Pages get a specific button.
+		if ( subjectType === SubjectType.Page ) {
+			importButtons.push( {
+				text: `Import Pages`,
+				url: Screens.importPagesStart( session.id ),
+			} );
+			return;
+		}
+
+		const schema = schemas[ subjectType ];
+		importButtons.push( {
+			text: `Import ${ schema.title }s`,
+			url: Screens.blueprints.new( session.id, schema.slug ),
+		} );
+	} );
 
 	return (
 		<>
