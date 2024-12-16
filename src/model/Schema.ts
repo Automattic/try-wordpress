d462078827079898b37010b57ea90f0f0f20ac3a
Paulo Pinto <paulo.pinto@automattic.com>
2024-12-16 14:33:47 +0000

    Typehint return value of getSchema()

--- src/model/Schema.ts
+++ src/model/Schema.ts
@@ -1,10 +1,22 @@
 // eslint-disable-next-line import/no-unresolved
-import Schema from '@schema/schema.json';
+import SchemasJson from '@schema/schema.json';
 import { SubjectType } from '@/model/Subject';
 
-export function getSchema( subjectType: SubjectType ) {
-	if ( ! Schema.hasOwnProperty( subjectType ) ) {
+interface Schemas {
+	[ key: string ]: Schema;
+}
+
+interface Schema {
+	title: string;
+	slug: string;
+	fields: Record< string, any >;
+}
+
+const schemas: Schemas = SchemasJson as Schemas;
+
+export function getSchema( subjectType: SubjectType ): Schema {
+	if ( ! schemas.hasOwnProperty( subjectType ) ) {
 		throw new Error( `Unknown subjectType: ${ subjectType }` );
 	}
-	return Schema[ subjectType ];
+	return schemas[ subjectType ];
 }
