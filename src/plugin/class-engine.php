af1ebce4730401d3c8c3aaed12671584ff272346
ashfame <mail@ashfame.com>
2024-12-27 13:27:27 +0400

    load observers registry + reorder require calls

--- src/plugin/class-engine.php
+++ src/plugin/class-engine.php
@@ -8,16 +8,21 @@ class Engine {
 
 	public function __construct() {
 		require 'enum-subject-type.php';
+		require 'class-subject.php';
+		require 'class-schema.php';
 
 		require 'class-handlers-registry.php';
+		require 'class-observers-registry.php';
+
+		require 'class-subjects-controller.php';
+
+		require 'class-storage.php';
+
 		require 'class-post-type-ui.php';
 		require 'class-transformer.php';
-		require 'class-subjects-controller.php';
-		require 'class-storage.php';
-		require 'class-schema.php';
-		require 'utils.php';
-		require 'class-subject.php';
 		require 'class-ops.php';
+
+		require 'utils.php';
 
 		( function () {
 			new Transformer();
